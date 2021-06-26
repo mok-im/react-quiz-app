7783de87f0efcaa0600b7d40924e82e5db087197
Igor <igorflyiq442@gmail.com>
2021-06-26 17:32:45 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import Layout from "./hoc/Layout/Layout"
+import Quiz from './containers/Quiz/Quiz';
+
+class App extends Component {
+  render() {
+    return (
+      <Layout>
+
+
+        <Quiz>
+        </Quiz>
+      </Layout>
+    )
+  }
+}
+
+
+export default App;
